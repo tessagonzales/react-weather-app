17cc5236eec7699a7257a0795b66ce45be378e26
Tessa Gonzales <tessaleego@gmail.com>
2018-04-05 15:20:13 -0700

    added search

--- src/App.js
+++ src/App.js
@@ -28,13 +28,27 @@ class App extends Component {
     })
   }
 
+  changeCity = (city) => {
+    Promise.all([
+      axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=imperial`),
+      axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=imperial`)    ])
+    .then(result => {
+      //console.log('on change', result)
+      let [ current, fiveDay ] = result
+      this.setState({
+        currWeather: current.data,
+        fiveDay: fiveDay.data,
+      })
+    })
+  }
+
   render() {
     //console.log('Phx Current Weather:', this.state.currWeather)
     //console.log('Phx FiveDay:', this.state.fiveDay)
     //console.log('api key', process.env.REACT_APP_API_KEY);
     return (
       <div className="App">
-        <SearchBar />
+        <SearchBar changeCityFunc={this.changeCity}/>
         <Current
           currWeatherTemp={this.state.currWeather.main}
           cityName={this.state.currWeather.name}
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 
 class Searchbar extends Component {
 
+  state = {
+    city: ''
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.changeCityFunc(this.state.city)
+  }
+
 
   render() {
     let fontColor = {
@@ -11,9 +20,15 @@ class Searchbar extends Component {
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <h1 style={fontColor}>Weather Time</h1>
 
-      <form className="form-inline my-2 my-lg-0">
+      <form
+        onSubmit = {this.handleSubmit}
+        className="form-inline my-2 my-lg-0">
         <div className="form-group mx-sm-3 mb-2">
-          <input type="text" className="form-control" placeholder="ex: London" />
+          <input
+            type="text"
+            className="form-control"
+            placeholder="ex: London"
+            onChange = {e => this.setState({city:e.target.value})}/>
         </div>
       <button type="submit" className="btn btn-primary mb-2">Search City</button>
     </form>
